103b4a773301a3a22fd7dfe2f3d234cb2c95b668
IgorPerikov <perikov.igor@gmail.com>
2016-02-13 14:19:09 +0300

    remove test classes

--- src/main/groovy/com/squesh/deloris/server/WebSocketConfig.groovy
+++ src/main/groovy/com/squesh/deloris/server/WebSocketConfig.groovy
@@ -2,7 +2,6 @@ package com.squesh.deloris.server
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
@@ -11,27 +10,16 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 @Configuration
 @EnableWebSocketMessageBroker
 @EnableWebSocket
-@CrossOrigin
 class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic")
+        config.setApplicationDestinationPrefixes("/app")
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/hello").setAllowedOrigins("*").withSockJS();
+    void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/entry").setAllowedOrigins("*").withSockJS()
     }
-
-//    @Override
-//    void configureMessageBroker(MessageBrokerRegistry config) {
-//        config.enableSimpleBroker("/topic")
-//    }
-//
-//    @Override
-//    void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/get-players", "/move-player")
-//    }
 }
