1a6465d6bd3ebfb38c1c78ed5ffe7c4ed446045a
IgorPerikov <perikov.igor@gmail.com>
2016-02-15 22:36:46 +0300

    add simple cleaner for "dead" users

--- src/main/groovy/com/squesh/deloris/server/server/service/PlayerService.groovy
+++ src/main/groovy/com/squesh/deloris/server/server/service/PlayerService.groovy
@@ -4,10 +4,19 @@ import com.squesh.deloris.server.core.Player
 import com.squesh.deloris.server.server.message.PlayerMovementMessage
 import org.springframework.stereotype.Service
 
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 @Service
 class PlayerService {
     List<Player> players = []
     def random = new Random()
+
+    def executorService = Executors.newScheduledThreadPool(2)
+
+    PlayerService() {
+        executorService.scheduleAtFixedRate({players = []}, 1, 1, TimeUnit.HOURS)
+    }
 
     Player registerPlayer(String name) {
         def player = new Player(UUID.randomUUID().toString(), name, random.nextInt(20), random.nextInt(20))
