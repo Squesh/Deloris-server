c7565981c16e14d8b83eb516472a6dd7bce16407
IgorPerikov <perikov.igor@gmail.com>
2016-02-21 22:20:41 +0300

    gstring -> string

--- src/main/groovy/com/squesh/deloris/server/server/service/GlobalMapService.groovy
+++ src/main/groovy/com/squesh/deloris/server/server/service/GlobalMapService.groovy
@@ -1,9 +1,7 @@
 package com.squesh.deloris.server.server.service
 
-import com.squesh.deloris.server.core.Hero
 import com.squesh.deloris.server.server.message.HeroMovementMessage
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 
@@ -16,12 +14,12 @@ class GlobalMapService {
     SimpMessagingTemplate template;
 
     void getHeroes() {
-        template.convertAndSend("/topic/init-heroes", heroService.heroes)
+        template.convertAndSend('/topic/init-heroes', heroService.heroes)
     }
 
     void registerHero(String name) {
         def hero = heroService.registerHero(name)
-        template.convertAndSend("/topic/registering-hero", hero)
+        template.convertAndSend('/topic/registering-hero', hero)
     }
 
     void unregisterHero(String token) {
@@ -30,6 +28,6 @@ class GlobalMapService {
 
     void moveHero(HeroMovementMessage heroMovementMessage) {
         heroService.moveHero(heroMovementMessage)
-        template.convertAndSend("/topic/heroes", heroService.heroes)
+        template.convertAndSend('/topic/heroes', heroService.heroes)
     }
 }
