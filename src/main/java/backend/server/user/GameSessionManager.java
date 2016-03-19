f2bb8a75340ada85f8d579594d53527869e081b2
IgorPerikov <perikov.igor@gmail.com>
2016-03-19 23:20:35 +0300

    Fix tokens set initialization

--- src/main/java/backend/server/user/GameSessionManager.java
+++ src/main/java/backend/server/user/GameSessionManager.java
@@ -2,12 +2,13 @@ package backend.server.user;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class GameSessionManager {
-    private Set<UUID> tokens;
+    private Set<UUID> tokens = new HashSet<>();
 
     public UUID registerNewToken() {
         UUID uuid = UUID.randomUUID();
